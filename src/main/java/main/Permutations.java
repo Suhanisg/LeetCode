8dab163ccacb2ad658ac4d3f4db945d296c92881
Suhanisg <suhugoyal@gmail.com>
2024-03-18 23:35:21 +0530

    Permutations

---
+++ src/main/java/main/Permutations.java
@@ -0,0 +1,25 @@
+package main;
+
+public class Permutations {
+    class Solution {
+        public List<List<Integer>> permute(int[] nums) {
+            List<List<Integer>> l = new ArrayList<>();
+            backtrack(l, new ArrayList<>(), nums);
+            return l;
+        }
+        private void backtrack(List<List<Integer>> l, List<Integer> tempList, int [] nums){
+            if(tempList.size() == nums.length)
+            {
+                l.add(new ArrayList<>(tempList));
+            }
+            else{
+                for(int i = 0; i < nums.length; i++){
+                    if(tempList.contains(nums[i])) continue;
+                    tempList.add(nums[i]);
+                    backtrack(l, tempList, nums);
+                    tempList.remove(tempList.size() - 1);
+                }
+            }
+        }
+    }
+}
