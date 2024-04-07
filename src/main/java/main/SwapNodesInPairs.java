231c523b57ee1d460bb6f4e7c19565a138edc505
Suhanisg <suhugoyal@gmail.com>
2024-04-07 22:21:37 +0530

    SwapNodesInPairs

---
+++ src/main/java/main/SwapNodesInPairs.java
@@ -0,0 +1,29 @@
+package main;
+
+public class SwapNodesInPairs {
+    class Solution {
+        public ListNode swapPairs(ListNode head) {
+
+            ListNode d = new ListNode(-1);
+            ListNode prev = d;
+
+            if(head == null || head.next == null) return head;
+
+            while (head != null && head.next != null) {
+
+                ListNode firstNode = head;
+                ListNode secondNode = head.next;
+
+                prev.next = secondNode;
+                firstNode.next = secondNode.next;
+                secondNode.next = firstNode;
+
+                prev = firstNode;
+                head = firstNode.next;
+            }
+
+            return d.next;
+        }
+    }
+
+}
