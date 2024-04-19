f8829a00ff65617d3f1b750e04c49d0325c3725b
Suhanisg <suhugoyal@gmail.com>
2024-04-19 23:52:16 +0530

    HappyNumber

---
+++ src/main/java/main/HappyNumber.java
@@ -0,0 +1,35 @@
+package main;
+
+public class HappyNumber {
+    class Solution {
+
+        public boolean isHappy(int n) {
+            if (n == 1 || n == -1) {
+                return true;
+            }
+
+            Set<Integer> visit = new HashSet<Integer>();
+            while (!visit.contains(n)) {
+                visit.add(n);
+                n = sumOfSquare(n);
+
+                if (n == 1) return true;
+            }
+
+            return false;
+        }
+
+        public int sumOfSquare(int n) {
+            int output = 0;
+
+            while (n != 0) {
+                int digit = n % 10;
+                digit = digit * digit;
+                output += digit;
+                n = n / 10;
+            }
+            return output;
+        }
+    }
+
+}
