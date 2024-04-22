315e1bcc7b6e199c0ea8a0282f350b3580c0da61
Suhanisg <suhugoyal@gmail.com>
2024-04-22 23:17:03 +0530

    PathSumII

---
+++ src/main/java/main/PathSumII.java
@@ -0,0 +1,33 @@
+package main;
+
+public class PathSumII {
+    class Solution {
+
+        List<List<Integer>> a = new ArrayList<>();
+        public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+            if(root == null)
+                return a;
+
+            pathSum(root, 0, targetSum, new ArrayList<>());
+            return a;
+        }
+
+        private void pathSum(TreeNode root, int currSum, int targetSum, List<Integer> currList){
+
+            if(root == null){
+                return;
+            }
+
+            currList.add(root.val);
+
+            if(root.left == null && root.right == null && currSum + root.val == targetSum){
+                a.add(new ArrayList<>(currList));
+            }
+            else{
+                pathSum(root.left, currSum + root.val, targetSum, currList);
+                pathSum(root.right, currSum + root.val, targetSum, currList);
+            }
+            currList.remove(currList.size() - 1);
+        }
+    }
+}
