4d3a4b8f0fe7712c2c5037f452b4a1e7649951c8
Suhanisg <suhugoyal@gmail.com>
2024-05-16 21:33:48 +0530

    PowerOfThree

---
+++ src/main/java/main/PowerOfThree.java
@@ -0,0 +1,14 @@
+package main;
+
+public class PowerOfThree {
+    class Solution {
+        public boolean isPowerOfThree(int n) {
+
+            if (n == 1){return true;}
+            if (n%3 != 0 || n == 0) {
+                return false;
+            }
+            return isPowerOfThree(n/3);
+        }
+    }
+}
