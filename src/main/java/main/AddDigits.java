8138ff9f465b45a5d128b466909d97718ae48a46
Suhanisg <suhugoyal@gmail.com>
2024-05-27 20:34:46 +0530

    AddDigits

---
+++ src/main/java/main/AddDigits.java
@@ -0,0 +1,25 @@
+package main;
+
+public class AddDigits {
+    class Solution {
+        public int addDigits(int num) {
+
+            if(num % 10 == num)
+                return num;
+            else
+                return addDigits(sum(num));
+
+        }
+
+        public int sum(int num) {
+
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            return sum;
+        }
+
+    }
+}
