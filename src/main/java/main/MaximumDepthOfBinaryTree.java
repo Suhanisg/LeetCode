598075cdc7415a600982aa072020d7e24d593f82
Suhanisg <suhugoyal@gmail.com>
2024-06-03 21:54:23 +0530

    MaximumDepthOfBinaryTree

---
+++ src/main/java/main/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,11 @@
+package main;
+
+public class MaximumDepthOfBinaryTree {
+    class Solution {
+
+        public int maxDepth(TreeNode root) {
+            if (root == null) return 0;
+            return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+        }
+    }
+}
