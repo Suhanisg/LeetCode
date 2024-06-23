e6c275495ed37cf2effbe3171ce43ed99a27846a
Suhanisg <suhugoyal@gmail.com>
2024-06-23 21:55:51 +0530

    MissingNumber

---
+++ src/main/java/main/MissingNumber.java
@@ -0,0 +1,15 @@
+package main;
+
+public class MissingNumber {
+    class Solution {
+
+        public int missingNumber(int[] nums) {
+            int sum = 0;
+            int total = nums.length * (nums.length + 1) / 2;
+            for (int i = 0; i < nums.length; i++) {
+                sum += nums[i];
+            }
+            return total -sum;
+        }
+    }
+}
