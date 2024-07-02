2150578f7aa4acea656b34eb1f0c68fba3ded0cd
Suhanisg <suhugoyal@gmail.com>
2024-07-02 21:49:19 +0530

    HIndexii

---
+++ src/main/java/main/HIndexii.java
@@ -0,0 +1,15 @@
+package main;
+
+public class HIndexii {
+    class Solution {
+        public int hIndex(int[] c) {
+            int x=0;
+            for(int i=0, n=c.length, j=n-1, m=0; i<=j;x=i, m=(i+j)/2){
+                if(c[m]==n-m) return n-m;
+                if(c[m]<n-m) i=m+1;
+                else j=m-1;
+            }
+            return c.length-x;
+        }
+    }
+}
