00cda5d004b4d405d3bc55bf45fd2d783c064e5e
Suhanisg <suhugoyal@gmail.com>
2024-07-10 14:07:08 +0530

    ValidPerfectSquare

---
+++ src/main/java/main/ValidPerfectSquare.java
@@ -0,0 +1,24 @@
+package main;
+
+public class ValidPerfectSquare {
+    class Solution {
+        public boolean isPerfectSquare(int num) {
+
+            long left = 1;
+            long right = num;
+            while (left <= right) {
+
+                long mid = (left + right) / 2;
+                if (mid * mid == num) {
+                    return true;
+                } else if (mid * mid > num) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+
+            }
+            return false;
+        }
+    }
+}
