3611634ff30f66b9833a5179ee656756729fb49b
Suhanisg <suhugoyal@gmail.com>
2024-07-20 21:36:19 +0530

    SFirstUniqueCharacterInAString

---
+++ src/main/java/main/SFirstUniqueCharacterInAString.java
@@ -0,0 +1,26 @@
+package main;
+
+public class SFirstUniqueCharacterInAString {
+    class Solution {
+        public int firstUniqChar(String s) {
+
+            if(s.length() == 1) {
+                return 0;
+            }
+
+            int[] counter = new int[26];
+
+            for(char c : s.toCharArray()) {
+                counter[c - 'a']++;
+            }
+
+            for(char c : s.toCharArray()) {
+                if(counter[c - 'a'] == 1) {
+                    return s.indexOf(c);
+                }
+            }
+
+            return -1;
+        }
+    }
+}
