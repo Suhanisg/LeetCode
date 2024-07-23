e7de4f32cf01e46eb516cdb5208ff7869fce2ccb
Suhanisg <suhugoyal@gmail.com>
2024-07-23 21:28:55 +0530

    DecodeString

---
+++ src/main/java/main/DecodeString.java
@@ -0,0 +1,38 @@
+package main;
+
+public class DecodeString {
+    class Solution {
+
+        int index = 0;
+
+        public String decodeString(String s) {
+
+            StringBuilder decoded = new StringBuilder();
+            while (index < s.length() && s.charAt(index) != ']') {
+
+
+                if (!Character.isDigit(s.charAt(index))) decoded.append(s.charAt(index++));
+
+
+                else {
+                    int k = 0;
+
+
+                    while (index < s.length() && Character.isDigit(s.charAt(index))) k = k * 10 + s.charAt(index++) - '0';
+
+
+                    index++;
+                    String answer = decodeString(s);
+
+
+                    index++;
+
+
+                    while (k-- > 0) decoded.append(answer);
+                }
+            }
+            return new String(decoded);
+        }
+    }
+
+}
