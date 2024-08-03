293c9e9afb9272c41910e341ad7f6a7443f33d4f
Suhanisg <suhugoyal@gmail.com>
2024-08-03 20:43:07 +0530

    SIntegerBreak

---
+++ src/main/java/main/SIntegerBreak.java
@@ -0,0 +1,20 @@
+package main;
+
+public class SIntegerBreak {
+    class Solution {
+        public int integerBreak(int n) {
+            if (n < 4) return n - 1;
+
+            int res = 1;
+
+            while (n > 4) {
+                n -= 3;
+                res *= 3;
+            }
+
+            res *= n;
+            return res;
+        }
+    }
+}
+
