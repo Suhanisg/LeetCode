311cecaf7494e6d49c8b5fc94141a2e854702165
Suhanisg <suhugoyal@gmail.com>
2024-08-11 22:23:21 -0300

    RepeatedSubstringPattern

---
+++ src/main/java/main/RepeatedSubstringPattern.java
@@ -0,0 +1,12 @@
+package main;
+
+public class RepeatedSubstringPattern {
+    public class Solution {
+        public boolean repeatedSubstringPattern(String s) {
+            String doubled = s + s;
+            String sub = doubled.substring(1, doubled.length() - 1);
+            return sub.contains(s);
+        }
+    }
+
+}
