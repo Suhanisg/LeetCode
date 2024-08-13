f8a49017b603cd3d11ddf761ce9cb679d043c2f5
Suhanisg <suhugoyal@gmail.com>
2024-08-12 22:16:52 -0300

    InvertBinaryTree

---
+++ src/main/java/main/InvertBinaryTree.java
@@ -0,0 +1,14 @@
+package main;
+
+public class InvertBinaryTree {
+    class Solution {
+
+        public TreeNode invertTree(TreeNode root) {
+            if (root == null) return null;
+            TreeNode node = new TreeNode(root.val);
+            node.right = invertTree(root.left);
+            node.left = invertTree(root.right);
+            return node;
+        }
+    }
+}
