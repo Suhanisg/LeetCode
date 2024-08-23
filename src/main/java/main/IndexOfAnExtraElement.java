5f489cd8fe3d7e510c8627b55615826bbbf8603e
Suhanisg <suhugoyal@gmail.com>
2024-08-22 22:40:40 -0300

    IndexOfAnExtraElement

---
+++ src/main/java/main/IndexOfAnExtraElement.java
@@ -0,0 +1,14 @@
+package main;
+
+public class IndexOfAnExtraElement {
+    class Solution {
+        public int findExtra(int n, int arr1[], int arr2[]) {
+            // add code here.
+            for(int i=0;i<arr1.length-1;i++){
+                if(arr1[i] != arr2[i]) return i;
+            }
+            return arr1.length-1;
+
+        }
+    }
+}
